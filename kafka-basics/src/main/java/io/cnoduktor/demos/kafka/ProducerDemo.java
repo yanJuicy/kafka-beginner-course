bdee982b2a2a17433c338eb3f52a85c75c4663df
yanJuicy <hanjub3@gmail.com>
2023-05-20 23:11:24 +0900

    Java 프로듀서

--- kafka-basics/src/main/java/io/cnoduktor/demos/kafka/ProducerDemo.java
+++
@@ -1,8 +0,0 @@
-package io.cnoduktor.demos.kafka;
-
-public class ProducerDemo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,44 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create producer properties
+        Properties properties = new Properties();
+
+        // connect to Conduktor Playground
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"\" password=\"\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create a Producer Reord
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(producerRecord);
+        producer.flush();
+        producer.close();
+    }
+}
